6abd9c2478c211b331e0bbe073db01e23b60b49f
Nick Pezza <pezza@hey.com>
2025-07-21 20:09:44 -0400

    Add missing require

--- lib/active_client.rb
+++ lib/active_client.rb
@@ -2,6 +2,7 @@ require "active_client/version"
 require "active_client/railtie"
 require "active_client/log_subscriber"
 require "active_client/base"
+require "net/http/persistent"
 
 module ActiveClient
 end
